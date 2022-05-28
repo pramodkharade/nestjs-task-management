ae6ddd9c1022cfef02447384896eb72e43858166
Pramod Kharade <khara@pc-pramodk.secure.rrootshell.com>
2022-05-29 00:45:20 +0530

    created DTO and mapped with controller body and service

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,4 +1,5 @@
 import { Body, Controller, Get, Post } from '@nestjs/common';
+import { CreateTaskDto } from './dto/create-task.dto';
 import { Task } from './task.model';
 import { TasksService } from './tasks.service';
 
@@ -11,10 +12,7 @@ export class TasksController {
   }
 
   @Post()
-  createTask(
-    @Body('title') title: string,
-    @Body('description') description: string,
-  ): Task {
-    return this.tasksService.createTask(title, description);
+  createTask(@Body() createtaskdto: CreateTaskDto): Task {
+    return this.tasksService.createTask(createtaskdto);
   }
 }
--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { Task, TaskStatus } from './task.model';
 import { v4 as uuidNumber } from 'uuid';
+import { CreateTaskDto } from './dto/create-task.dto';
 @Injectable()
 export class TasksService {
   private tasks: Task[] = [];
@@ -9,7 +10,8 @@ export class TasksService {
     return this.tasks;
   }
 
-  createTask(title: string, description: string): Task {
+  createTask(createtaskdto: CreateTaskDto): Task {
+    const { title, description } = createtaskdto;
     const task: Task = {
       id: uuidNumber(),
       title,
