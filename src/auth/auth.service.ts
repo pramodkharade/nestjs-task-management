58fbc51b072e7545dc60be04305d41f8da1a7014
Pramod Kharade <khara@pc-pramodk.secure.rrootshell.com>
2022-06-02 22:48:51 +0530

    User DTO,respo method and singup

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
+import { AuthCredentialsDto } from './dto/auth-credentials.dto';
 import { UserRepository } from './users.repository';
 
 @Injectable()
@@ -8,4 +9,8 @@ export class AuthService {
     @InjectRepository(UserRepository)
     private userRespository: UserRepository,
   ) {}
+
+  async signUp(authCredentialDto: AuthCredentialsDto): Promise<void> {
+    return this.userRespository.createUser(authCredentialDto);
+  }
 }
--- src/auth/users.repository.ts
+++ src/auth/users.repository.ts
@@ -1,8 +1,12 @@
-import { EntityRepository, Repository } from "typeorm";
-import { User } from "./user.entity";
-
+import { EntityRepository, Repository } from 'typeorm';
+import { AuthCredentialsDto } from './dto/auth-credentials.dto';
+import { User } from './user.entity';
 
 @EntityRepository(User)
-export class UserRepository extends Repository<User>{
-    
+export class UserRepository extends Repository<User> {
+  async createUser(authCredentialDto: AuthCredentialsDto): Promise<void> {
+    const { username, password } = authCredentialDto;
+    const user = this.create({ username, password });
+    await this.save(user);
+  }
 }
