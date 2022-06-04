9158df9b61ec719bc980f341454a12e1d596502c
Pramod Kharade <khara@pc-pramodk.secure.rrootshell.com>
2022-06-04 19:30:39 +0530

    bcrypt and hased password

--- src/auth/users.repository.ts
+++ src/auth/users.repository.ts
@@ -5,12 +5,14 @@ import {
 import { EntityRepository, Repository } from 'typeorm';
 import { AuthCredentialsDto } from './dto/auth-credentials.dto';
 import { User } from './user.entity';
-
+import * as bcrypt from 'bcrypt';
 @EntityRepository(User)
 export class UserRepository extends Repository<User> {
   async createUser(authCredentialDto: AuthCredentialsDto): Promise<void> {
     const { username, password } = authCredentialDto;
-    const user = this.create({ username, password });
+    const salt = await bcrypt.genSalt();
+    const hasedPassword = await bcrypt.hash(password, salt);
+    const user = this.create({ username, password: hasedPassword });
     try {
       await this.save(user);
     } catch (error) {
