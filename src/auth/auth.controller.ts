b4bc651f53fe6b68789391b64a2b68d5623bb721
Pramod Kharade <khara@pc-pramodk.secure.rrootshell.com>
2022-06-04 19:45:42 +0530

    sign in impl

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -5,8 +5,13 @@ import { AuthCredentialsDto } from './dto/auth-credentials.dto';
 @Controller('auth')
 export class AuthController {
   constructor(private authService: AuthService) {}
+ 
   @Post('/singup')
   signUp(@Body() authCredentialDto: AuthCredentialsDto): Promise<void> {
     return this.authService.signUp(authCredentialDto);
+  }
+  @Post('/signin')
+  signIn(@Body() authCredentialDto: AuthCredentialsDto): Promise<string> {
+    return this.authService.signIn(authCredentialDto);
   }
 }
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,8 +1,8 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { AuthCredentialsDto } from './dto/auth-credentials.dto';
 import { UserRepository } from './users.repository';
-
+import * as bcrypt from 'bcrypt';
 @Injectable()
 export class AuthService {
   constructor(
@@ -12,5 +12,15 @@ export class AuthService {
 
   async signUp(authCredentialDto: AuthCredentialsDto): Promise<void> {
     return this.userRespository.createUser(authCredentialDto);
+  }
+
+  async signIn(authCredentialDto: AuthCredentialsDto): Promise<string> {
+    const { username, password } = authCredentialDto;
+    const user = await this.userRespository.findOne({ username: username });
+    if (user && (await bcrypt.compare(password, user.password))) {
+      return 'success';
+    } else {
+      throw new UnauthorizedException('Please check your login credentials');
+    }
   }
 }
