b6f46dff0082c198507517ac7b1a6fb630e371e0
Pramod Kharade <khara@pc-pramodk.secure.rrootshell.com>
2022-06-04 22:44:49 +0530

    Tasks and user realtion in DB

--- src/auth/get-user.decorator.ts
+++ src/auth/get-user.decorator.ts
@@ -2,7 +2,7 @@ import { createParamDecorator, ExecutionContext } from '@nestjs/common';
 import { User } from './user.entity';
 
 export const GetUser = createParamDecorator(
-  (data, ctx: ExecutionContext): User => {
+  (_data, ctx: ExecutionContext): User => {
     const req = ctx.switchToHttp().getRequest();
     return req.user;
   },
--- src/tasks/task.entity.ts
+++ src/tasks/task.entity.ts
@@ -1,4 +1,6 @@
-import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+import { userInfo } from 'os';
+import { User } from 'src/auth/user.entity';
+import { Column, Entity, ManyToOne, PrimaryGeneratedColumn } from 'typeorm';
 import { TaskStatus } from './task-status.enum';
 
 @Entity()
@@ -14,4 +16,7 @@ export class Task {
 
   @Column()
   status: TaskStatus;
+
+  @ManyToOne((_type) => User, (user) => user.tasks, { eager: false })
+  user: User;
 }
