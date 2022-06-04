64f1a3c2c43bfb6c2a34c913c8701960d76b6999
Pramod Kharade <khara@pc-pramodk.secure.rrootshell.com>
2022-06-04 20:37:38 +0530

    sign and generate token

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -5,13 +5,15 @@ import { AuthCredentialsDto } from './dto/auth-credentials.dto';
 @Controller('auth')
 export class AuthController {
   constructor(private authService: AuthService) {}
- 
+
   @Post('/singup')
   signUp(@Body() authCredentialDto: AuthCredentialsDto): Promise<void> {
     return this.authService.signUp(authCredentialDto);
   }
   @Post('/signin')
-  signIn(@Body() authCredentialDto: AuthCredentialsDto): Promise<string> {
+  signIn(
+    @Body() authCredentialDto: AuthCredentialsDto,
+  ): Promise<{ accessToken: string }> {
     return this.authService.signIn(authCredentialDto);
   }
 }
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -3,22 +3,29 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { AuthCredentialsDto } from './dto/auth-credentials.dto';
 import { UserRepository } from './users.repository';
 import * as bcrypt from 'bcrypt';
+import { JwtService } from '@nestjs/jwt';
+import { JwtPayLoad } from './awt-payload.interface';
 @Injectable()
 export class AuthService {
   constructor(
     @InjectRepository(UserRepository)
     private userRespository: UserRepository,
+    private jwtService: JwtService,
   ) {}
 
   async signUp(authCredentialDto: AuthCredentialsDto): Promise<void> {
     return this.userRespository.createUser(authCredentialDto);
   }
 
-  async signIn(authCredentialDto: AuthCredentialsDto): Promise<string> {
+  async signIn(
+    authCredentialDto: AuthCredentialsDto,
+  ): Promise<{ accessToken: string }> {
     const { username, password } = authCredentialDto;
     const user = await this.userRespository.findOne({ username: username });
     if (user && (await bcrypt.compare(password, user.password))) {
-      return 'success';
+      const payload: JwtPayLoad = { username };
+      const accessToken: string = await this.jwtService.sign(payload);
+      return { accessToken };
     } else {
       throw new UnauthorizedException('Please check your login credentials');
     }
